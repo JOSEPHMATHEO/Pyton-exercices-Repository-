a5e625fa2d46d3f74ac71c88632cd7df66e4e267
JOSEPHMATHEO <91102368+JOSEPHMATHEO@users.noreply.github.com>
2023-03-17 17:57:51 -0500

    chnages

--- programa Udemy 10 Pyton.py
+++ programa Udemy 10 Pyton.py
@@ -1,6 +1,6 @@
 import matplotlib.pyplot as plt
 
-print("Frecuencia de Pajas en la Semana")
+print("Frecuencia de de horas jugadas en la Semana")
 print("================================")
 x = [1,2,3,4,5,6,7]
 y = [2,3,0,1,2,3,4]
@@ -11,5 +11,5 @@ plt.xticks(x,dias)
 plt.plot(x,y)
 plt.bar(x,y)
 plt.ylabel("Dias de la Semana")
-plt.title("Frecuencia de Pajas en la Semana")
+plt.title("Frecuencia de horas jugadas en la Semana")
 plt.show()
